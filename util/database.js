613b0f53fb20105a04dfe4de0e91b53d921c07f2
nfr2208 <nfr2208@gmail.com>
2019-05-02 18:49:18 +0700

    test deployment amd

--- util/database.js
+++ util/database.js
@@ -2,12 +2,8 @@ const Sequelize = require('sequelize');
 
 const sequelize = new Sequelize('amddigit_demo_amd','amddigit_naufal','P$ps2xbox', {
     dialect: 'mysql',
-<<<<<<< HEAD
     host: '45.64.99.177',
     port: '3306'
-=======
-    host: '10.107.30.151'
->>>>>>> dde56a48ff773e2eede5d66ded9704577a8c5782
 });
 
 // DB_CONNECTION=mysql
