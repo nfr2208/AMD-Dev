7b7562d06f2de8cc0b920bc01deb91792b0f0cea
nfr2208 <nfr2208@gmail.com>
2019-05-02 18:41:48 +0700

    test deployment

--- util/database.js
+++ util/database.js
@@ -1,8 +1,16 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('amd-db','root','admin', {
+const sequelize = new Sequelize('amddigit_demo_amd','amddigit_naufal','P$ps2xbox', {
     dialect: 'mysql',
-    host: 'localhost'
+    host: '45.64.99.177',
+    port: '3306'
 });
+
+// DB_CONNECTION=mysql
+// DB_HOST=45.64.99.177
+// DB_PORT=3306
+// DB_DATABASE=amddigit_database_amd
+// DB_USERNAME=amddigit_betteng
+// DB_PASSWORD=SAM##sam2018
 
 module.exports = sequelize;
