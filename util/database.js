747d6d9fe9a7948bb99c212a4e1c96737465637e
nfr2208 <nfr2208@gmail.com>
2019-05-08 15:45:37 +0700

    perubahan view pada view amoeba untuk digunakan team data

--- util/database.js
+++ util/database.js
@@ -1,8 +1,16 @@
 const Sequelize = require('sequelize');
 
+// Development
 const sequelize = new Sequelize('amd-db','root','admin', {
     dialect: 'mysql',
     host: 'localhost'
 });
+
+// Demo deployment
+// const sequelize = new Sequelize('amddigit_demo_amd','amddigit_naufal','P$ps2xbox', {
+//     dialect: 'mysql',
+//     host: '45.64.99.177',
+//     port: '3306'
+// });
 
 module.exports = sequelize;
