a0550a0e6526cb662fcaf47da6fa3cef4ca040ad
nfr2208 <nfr2208@gmail.com>
2019-05-16 09:38:35 +0700

    fix alpha merge dari moses

--- controllers/menuController.js
+++ controllers/menuController.js
@@ -1,13 +1,20 @@
 exports.getDashboard = (req, res, next) => {
     res.render('index', {
-        path: '/',
+        path: '/index',
         pageTitle: 'Dashboard'
     });
 };
 
 exports.getInner = (req, res, next) => {
-    res.render('login-5', {
+    res.render('inner', {
         path: '/inner',
         pageTitle: 'Inner Dashboard'
+    });
+};
+
+exports.getLogin = (req, res, next) => {
+    res.render('login', {
+        path: '/login',
+        pageTitle: 'Login'
     });
 };
