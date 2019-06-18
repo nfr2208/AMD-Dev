66da66e03c6cbd5e4ec6bdfc829261761cb42b71
unknown <mosesatasoge58@gmail.com>
2019-06-18 13:17:00 +0700

    penambahan model amoeba

---
+++ models/amoeba/CFU.js
@@ -0,0 +1,18 @@
+const Sequelize = require('sequelize');
+
+const sequelize = require('../../util/database');
+
+const CFU = sequelize.define('CFU', {
+    Id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    CFU: {
+        type: Sequelize.STRING,
+        allowNull: false
+    }
+});
+
+module.exports = CFU;
--- routes/old/amoebaRoutes.js
+++ routes/old/amoebaRoutes.js
@@ -4,15 +4,15 @@ const amoebaController = require('../../controllers/old/amoebaController');
 
 const router = express.Router();
 
-router.get('/register-amoeba', amoebaController.getRegisterAmoeba);
-router.post('/register-amoeba', amoebaController.postRegisterAmoeba);
-router.get('/view-amoeba', amoebaController.getAmoebas);
-router.get('/edit-amoeba/:idAmoeba', amoebaController.getEditAmoeba);
-router.post('/edit-amoeba', amoebaController.postEditAmoeba);
-router.post('/delete-amoeba', amoebaController.postDeleteAmoeba);
-router.get('/detail-amoeba/:idAmoeba', amoebaController.getDetailAmoeba);
+// router.get('/register-amoeba', amoebaController.getRegisterAmoeba);
+// router.post('/register-amoeba', amoebaController.postRegisterAmoeba);
+// router.get('/view-amoeba', amoebaController.getAmoebas);
+// router.get('/edit-amoeba/:idAmoeba', amoebaController.getEditAmoeba);
+// router.post('/edit-amoeba', amoebaController.postEditAmoeba);
+// router.post('/delete-amoeba', amoebaController.postDeleteAmoeba);
+// router.get('/detail-amoeba/:idAmoeba', amoebaController.getDetailAmoeba);
 
-router.get('/api/amoebas', amoebaController.getAmoebasApi);
-router.post('/view-amoeba/upload', amoebaController.postUploadAmoeba);
+router.get('/api/amoebas', amoebaController.getAmoebaAPI);
+// router.post('/view-amoeba/upload', amoebaController.postUploadAmoeba);
 
 module.exports = router;
